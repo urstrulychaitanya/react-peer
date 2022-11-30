3b24bc5aaa98209d0829f749ed13754808f2e4c9
urstrulychaitanya <88933985+urstrulychaitanya@users.noreply.github.com>
2022-11-30 12:07:32 +0530

    1 file modified

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Main from './Main';
+
 export default function SignUp() {
   //State
   const [SignUpValues, setSignUpValues] = useState({});
